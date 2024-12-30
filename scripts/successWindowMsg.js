7eda2ee19ef88d63cd5888446a4e1bdd1c95abd3
Styri <styltia@hotmail.com>
2024-12-30 12:45:22 +0200

    Change success-message position.

--- scripts/successWindowMsg.js
+++ scripts/successWindowMsg.js
@@ -1,15 +1,17 @@
-        window.onload = function () {
-        // check url query to see if the form was submitted or not
-        const urlParams = new URLSearchParams(window.location.search);
-        // if it was, then show success message
-        if (urlParams.get("redirect") === "true") {
-          const successMessage = document.getElementById("success-message");
-          successMessage.classList.remove('translate-x-full', 'opacity-0');
-          successMessage.classList.add('translate-x-0', 'opacity-100');
-
-          setTimeout(function () {
-            successMessage.classList.add('translate-x-full', 'opacity-0');
-            successMessage.classList.remove('translate-x-0', 'opacity-100');
-          }, 3500);
-        }
-      };
+window.onload = function () {
+  // check url query to see if the form was submitted or not
+  const urlParams = new URLSearchParams(window.location.search);
+  const successMessage = document.getElementById("success-message");
+  // if it was submitted, show the message
+  if (urlParams.get("redirect") === "true") {
+    successMessage.classList.remove('translate-x-full');
+    successMessage.classList.add('opacity-100');
+  
+    setTimeout(function () {
+      successMessage.classList.add('translate-x-full');
+      successMessage.classList.remove('opacity-100');
+    }, 3500);
+    
+    window.history.replaceState({}, document.title, window.location.pathname);
+  }
+};
