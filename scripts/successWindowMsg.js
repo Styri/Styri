634660d6bd422972be59d8bf49726f2c6a262a88
Styri <styltia@hotmail.com>
2024-12-30 12:08:00 +0200

    Success message fix

--- scripts/successWindowMsg.js
+++ scripts/successWindowMsg.js
@@ -4,9 +4,12 @@
         // if it was, then show success message
         if (urlParams.get("redirect") === "true") {
           const successMessage = document.getElementById("success-message");
-          successMessage.classList.remove("hidden");
+          successMessage.classList.remove('translate-x-full', 'opacity-0');
+          successMessage.classList.add('translate-x-0', 'opacity-100');
+
           setTimeout(function () {
-            successMessage.classList.add("hidden");
+            successMessage.classList.add('translate-x-full', 'opacity-0');
+            successMessage.classList.remove('translate-x-0', 'opacity-100');
           }, 3500);
         }
       };
